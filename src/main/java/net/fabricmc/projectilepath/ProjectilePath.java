5826fe56c0b38f20bbb471e9024ff6412417836f
Xithrius <xithrius@gmail.com>
2023-03-26 23:02:23 -0700

    Event for punching a block without required tool

--- src/main/java/net/fabricmc/projectilepath/ProjectilePath.java
+++ src/main/java/net/fabricmc/projectilepath/ProjectilePath.java
@@ -1,6 +1,11 @@
 package net.fabricmc.projectilepath;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.player.AttackBlockCallback;
+import net.minecraft.block.BlockState;
+import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.math.Vec3d;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -10,12 +15,25 @@ public class ProjectilePath implements ModInitializer {
     // That way, it's clear which mod wrote info, warnings, and errors.
     public static final Logger LOGGER = LoggerFactory.getLogger("projectilepath");
 
+    /**
+     * This code runs as soon as Minecraft is in a mod-load-ready state.
+     * However, some things (like resources) may still be uninitialized.
+     * Proceed with mild caution.
+     */
     @Override
     public void onInitialize() {
-        // This code runs as soon as Minecraft is in a mod-load-ready state.
-        // However, some things (like resources) may still be uninitialized.
-        // Proceed with mild caution.
+        AttackBlockCallback.EVENT.register((player, world, hand, pos, direction) ->
+        {
+            BlockState state = world.getBlockState(pos);
 
-        LOGGER.info("Hello Fabric world!");
+            if (state.isToolRequired() && !player.isSpectator() &&
+                    player.getMainHandStack().isEmpty()) {
+                Vec3d position = player.getPos();
+                player.teleport(position.getX(), position.getY() + 5, position.getZ());
+                LOGGER.info(position.toString());
+            }
+
+            return ActionResult.PASS;
+        });
     }
 }
